17693d1aa5a17c05cd7e00dac01018243cb53df2
Thalles koester <thallesdella@gmail.com>
2021-04-29 17:32:36 -0300

    Add initial flags class prototype

---
+++ src/classes/flags.h
@@ -0,0 +1,19 @@
+//
+// Created by THALLES on 26/04/2021.
+//
+
+#ifndef GREP_FLAGS_H
+#define GREP_FLAGS_H
+
+#include "flag.h"
+
+namespace Grep {
+
+class Flags {
+public:
+
+};
+
+}
+
+#endif // GREP_FLAGS_H
