8576e0c7e78d873edcb4644c47b0c2ff2e00f03d
Thalles koester <thallesdella@gmail.com>
2021-04-29 18:21:32 -0300

    Add script entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
 
+#include "classes/controller.h"
+
+using Grep::Controller;
+
+int main(int argc, char *argv[]) {
+  auto *controller = new Controller(argc, argv);
+  controller->init();
+  delete controller;
+
+  return 0;
+}
