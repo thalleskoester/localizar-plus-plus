8b3570b2920027819564e4eca4cc8f03bac889f8
Thalles koester <thallesdella@gmail.com>
2021-04-29 17:37:39 -0300

    Add initial flags manager class prototype

---
+++ src/classes/flags-manager.h
@@ -0,0 +1,35 @@
+//
+// Created by THALLES on 22/04/2021.
+//
+
+#ifndef GREP_FLAGS_MANAGER_H
+#define GREP_FLAGS_MANAGER_H
+
+#include <string>
+#include <vector>
+
+#include "flag.h"
+
+using std::string;
+using std::vector;
+
+namespace Grep {
+
+class FlagsManager {
+  static const unsigned int MIN_ARGS{3};
+  int argc;
+  char **argv;
+  unsigned int active_flags;
+  vector<Flag *> *flags;
+
+public:
+  static const unsigned int FLAG_DEBUG{0};
+  static const unsigned int FLAG_HELP{1};
+
+  FlagsManager(int argc, char **argv);
+  ~FlagsManager();
+};
+
+} // namespace Grep
+
+#endif // GREP_FLAGS_MANAGER_H
