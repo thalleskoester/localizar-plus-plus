224b01693a443ef5e36d7c8c115b190a85a38787
Thalles koester <thallesdella@gmail.com>
2021-04-29 17:24:59 -0300

    Add flag status getter and setter implementations

--- src/classes/flag.cpp
+++ src/classes/flag.cpp
@@ -9,4 +9,8 @@ namespace Grep {
 Flag::Flag(string flag_name, string flag_desc, vector<string> flag_params)
     : name{flag_name}, desc{flag_desc}, params{flag_params}, status{false} {}
 
+void Flag::setStatus(bool status) { this->status = status; }
+
+bool Flag::getStatus() const { return this->status; }
+
 } // namespace Grep
