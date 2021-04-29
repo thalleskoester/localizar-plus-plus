224737e26642b3f894b328f408f6cb47f39982f1
Thalles koester <thallesdella@gmail.com>
2021-04-29 18:18:33 -0300

    Add controller parser abstraction implementation

--- src/classes/controller.cpp
+++ src/classes/controller.cpp
@@ -13,4 +13,15 @@ Controller::Controller(int arg_count, char **arg_vec)
 
 Controller::~Controller() { delete this->flagsManager; }
 
+bool Controller::parseArguments() {
+  if (!this->flagsManager->checkMinArgs(this->debug) ||
+      this->flagsManager->checkIfHelpFlagExists()) {
+    this->flagsManager->displayHelp();
+    return false;
+  }
+
+  this->flagsManager->parseFlagsFromArgs(this->debug);
+  return true;
+}
+
 } // namespace Grep
