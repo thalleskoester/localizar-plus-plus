7bc42b6fecd36d65ff2cbb814d674b0282f73f2e
Thalles koester <thallesdella@gmail.com>
2021-04-29 18:19:57 -0300

    Add controller init method implementation

--- src/classes/controller.cpp
+++ src/classes/controller.cpp
@@ -13,6 +13,14 @@ Controller::Controller(int arg_count, char **arg_vec)
 
 Controller::~Controller() { delete this->flagsManager; }
 
+void Controller::init() {
+  this->debug = this->flagsManager->checkIfDebugFlagExists();
+
+  if (!this->parseArguments()) {
+    return;
+  }
+}
+
 bool Controller::parseArguments() {
   if (!this->flagsManager->checkMinArgs(this->debug) ||
       this->flagsManager->checkIfHelpFlagExists()) {
