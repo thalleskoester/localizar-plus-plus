8a3870b61ece0f32f9f43a522a552a65ebc81f0f
Thalles koester <thallesdella@gmail.com>
2021-04-29 18:25:46 -0300

    Add the others flags factories implementation

--- src/classes/flags.cpp
+++ src/classes/flags.cpp
@@ -14,4 +14,32 @@ Flag *Flags::help() {
   return new Flag("Help", "Display help", {"-h", "--help"});
 }
 
+Flag *Flags::caseSensitive() {
+  return new Flag("Case Sensitive", "Case sensitive search disable",
+                  {"-i", "--case"});
+}
+
+Flag *Flags::count() {
+  return new Flag("Count",
+                  "Display count of lines with has appearances of the pattern",
+                  {"-c", "--count"});
+}
+
+Flag *Flags::lineNumber() {
+  return new Flag("Line Number",
+                  "Display the number of the line witch contains the pattern",
+                  {"-n", "--numb"});
+}
+
+Flag *Flags::occurrences() {
+  return new Flag("Occurrences",
+                  "Display occurrences os the pattern in the file",
+                  {"-r", "--rep"});
+}
+
+Flag *Flags::output() {
+  return new Flag("Output", "Save an output copy without the pattern",
+                  {"-o", "--out"});
+}
+
 }
